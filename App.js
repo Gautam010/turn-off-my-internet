f1c1b05d7987875da0d5bb25e5e15ecd06572468
Gautam Chaturvedi <Gautam010>
2024-11-14 08:49:31 +0530

    ADD: state variables for time picker

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Example = () => {
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [isTimeSet, setTime] = useState(false)
 
   const showTimePicker = () => {
     setTimePickerVisibility(true);
@@ -14,10 +15,22 @@ const Example = () => {
   };
 
   const handleConfirm = (time) => {
-    console.warn("A time has been picked: ", time.time);
+    console.warn("a time has been picked")
     hideTimePicker();
+    timeSetHandler(time);
+   
   };
 
+  const timeSetHandler = (time) => {
+    setTime(newTime)
+    // return(
+    //   <View style={{flex:1,justifyContent:"center",alignItems:"center",backgroundColor:"lightblue"}}>
+    //     <Text>Mobile data will be turned off at {String(time.getHours())}:{String(time.getMinutes().toString().padStart(2, '0'))}</Text>
+    //     <Button title="Change Time"/>
+    //   </View>
+    // )
+
+  }
   return (
     <View style={{flex:1,justifyContent: "center",alignItems: "center" }}>
       <Button title="Set Time to turn of mobile data" onPress={showTimePicker}/>
@@ -29,6 +42,10 @@ const Example = () => {
       />
     </View>
   );
+
+
+
 };
+
 
 export default Example;
