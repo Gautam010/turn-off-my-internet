17545fd616ef30e66408f1cfce39f048058b707f
Gautam Chaturvedi <Gautam010>
2024-11-17 11:58:55 +0530

    ADD: State added after time has been picked

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Example = () => {
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [time, setTime] = useState(null); 
 
   const showTimePicker = () => {
     setTimePickerVisibility(true);
@@ -13,14 +14,28 @@ const Example = () => {
     setTimePickerVisibility(false);
   };
 
-  const handleConfirm = (time) => {
-    console.warn("A time has been picked: ", time.time);
+  const handleConfirm = (selectedTime) => {
+    console.warn("A time has been picked: ", selectedTime);
+    setTime(selectedTime); 
     hideTimePicker();
   };
 
+
+  if (time) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "lightblue" }}>
+        <Text>
+          Mobile data will be turned off at {String(time.getHours())}:
+          {String(time.getMinutes().toString().padStart(2, "0"))}
+        </Text>
+        <Button title="Change Time" onPress={() => setTime(null)} />
+      </View>
+    );
+  }
+
   return (
-    <View style={{flex:1,justifyContent: "center",alignItems: "center" }}>
-      <Button title="Set Time to turn of mobile data" onPress={showTimePicker}/>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button title="Set Time to turn off mobile data" onPress={showTimePicker} />
       <DateTimePickerModal
         isVisible={isTimePickerVisible}
         mode="time"
